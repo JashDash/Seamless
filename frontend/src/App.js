5716b902d60b2bc1b6fffb9e085c4a495aa28466
Jashan Devgan <jashandevgan@gmail.com>
2023-02-23 22:14:52 +0530

    Dummy changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & crypto also!
         </a>
       </header>
     </div>
